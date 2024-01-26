8f9c8ab6a1e9bef9e973aa3ed9610767e2bb0b52
Cesar Tzaj <cesar.tzaj@outlook.com>
2024-01-25 20:26:27 -0600

    se agrego imagenes al proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,7 @@ module.exports = {
         'yo': '#243c5a',
       },
       container: {
-          sm: '315px',
-          md: '760px',
-          lg: '1023px',
-          xl: '1240px',
-          '2xl': '1536px',
+        center: true,
         },
     },
   },
